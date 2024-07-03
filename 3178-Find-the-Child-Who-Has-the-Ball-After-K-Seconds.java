46ab9af0d75d5696513764644b9fb07dde5e1d95
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2024-07-03 22:33:11 +0530

    [3178] [Time Beats: 100.00%] [Memory Beats: 92.22%] - LeetPush

---
+++ 3178-Find-the-Child-Who-Has-the-Ball-After-K-Seconds.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfChild(int n, int k) {
+        n--;
+        int round = k/n;
+        int rem = k%n;
+        if(round%2== 0){
+            return rem;
+        }
+        else{
+            return n-rem;
+        }
+
+    }
+}
