e2b0fdc1d3d16ebe2cd0f1e884f37db33c2c532e
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2024-08-19 12:08:13 +0530

    [2220] [Time Beats: 100.00%] [Memory Beats: 41.05%] - LeetPush

---
+++ 2220-Minimum-Bit-Flips-to-Convert-Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int ans = start ^ goal;
+        int cnt = 0 ;
+       while(ans != 0){
+            cnt += ans & 1;
+            ans >>= 1;
+        }
+        return cnt;
+    }
+}
