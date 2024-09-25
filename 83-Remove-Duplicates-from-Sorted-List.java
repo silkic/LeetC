ccaa76bcc81aa82d739e6f19b9093612018ab208
Silki Chakraborty <168343055+silkic@users.noreply.github.com>
2024-09-25 16:15:56 +0530

    [83] [Time Beats: 100.00%] [Memory Beats: 30.82%] - LeetPush

---
+++ 83-Remove-Duplicates-from-Sorted-List.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (fast != null) {
+            if (slow.val != fast.val) {
+                slow = slow.next;
+            }
+            else{
+                slow.next = fast.next;
+            }
+            fast = fast.next;
+        }
+        return head;
+    }
+}
